5e76558463409ce74012dba249037730b2e2c4ea
Shirshajit Sen Gupta <shirshajit@gmail.com>
2025-11-14 20:49:46 +0800

    remove ai slop

--- src/core/__init__.py
+++
@@ -1,6 +0,0 @@
-from .encoder import Encoder
-from .extractor import Extractor
-from .oie import OIE
-from .schema_definer import SchemaDefiner
-
-__all__ = ["Encoder", "Extractor", "OIE", "SchemaDefiner"]
